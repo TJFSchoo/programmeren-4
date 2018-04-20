3311f17dc745cdaee74d3f630bdcee51fcb62bc2
Tom Schoonbeek <tjfschoo@avans.nl>
2018-04-20 10:13:30 +0200

    test

--- les2/server.js
+++ les2/server.js
@@ -4,7 +4,15 @@ let port = 3000;
 http.createServer(function (request, response){
     console.log('Er was een request');
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write("Hello world!");
+
+
+    let result = {
+        firstName: Tom,
+        lastName: Schoonbeek,
+    };
+
+
+    response.write(JSON.stringify(result));
     response.end();
 }).listen(port);
 
